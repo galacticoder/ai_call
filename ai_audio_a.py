db20a3b97e4910eb2344025d691cda98e3d0314e
galactic <galacticoder@gmail.com>
2024-02-03 16:26:14 -0500

    made it so you can chat using mic only

--- ai_audio_a.py
+++ ai_audio_a.py
@@ -2,13 +2,41 @@ import shutil
 import requests
 from transformers import GPT2LMHeadModel, GPT2Tokenizer
 import os
+import assemblyai as aai
+import speech_recognition as sr
+from gtts import gTTS
+import torch
+from email_sender import *
+from mutagen.mp3 import MP3
+import time
+
+#if getting corrupt mp3 output then api is out of words
 
 with open("C:\\Users\\zombi\\OneDrive\\Desktop\\ai_call\\ai_call\\api.txt", "r") as api_file:
     lines = api_file.readlines()
     api_key = lines[0].strip()
     voice_id = lines[1].strip()
-    path = lines[3].strip()
-    api_text = lines[4].strip()
+    ai_path = lines[3].strip()
+    user_path = lines[4].strip()
+    api_assembly = lines[5].strip()
+
+def get_audio():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        audio = r.listen(source)
+        said = ""
+
+        try:
+            said = r.recognize_google(audio)
+            print(said)
+        except Exception as e:
+            print("Exception: " + str(e))
+
+    return said
+
+print("speak nigga we recording\n")
+text = get_audio()
+print("\nshut up nigga we not recording")
 
 model_name = "gpt2-xL"
 model = GPT2LMHeadModel.from_pretrained(model_name)
@@ -23,16 +51,18 @@ headers = {
     "xi-api-key": api_key
 }
 
-user_input = input("User: ")
+user_input = text
+
 input_ids = tokenizer.encode(user_input, return_tensors="pt")
-output = model.generate(input_ids, max_length=50, num_return_sequences=1, no_repeat_ngram_size=2)
+attention_mask = torch.ones(input_ids.shape, device=input_ids.device)  # Add this line to create attention mask
+output = model.generate(input_ids, max_length=300, num_return_sequences=1, no_repeat_ngram_size=2, attention_mask=attention_mask)
 generated_text = tokenizer.decode(output[0], skip_special_tokens=True)
 
 data = {
     "text": generated_text,
     "model_id": "eleven_monolingual_v1",
     "voice_settings": {
-        "stability": 0.65,
+        "stability": 0.75,
         "similarity_boost": 0.86
     }
 }
@@ -40,9 +70,21 @@ data = {
 response = requests.post(url, json=data, headers=headers)
 
 with open('ai_output.mp3', 'wb') as f:
-    for chunk in response.iter_content(chunk_size=CHUNK_SIZE):
-        if chunk:
-            f.write(chunk)
+    f.write(response.content)
             
-shutil.move('ai_output.mp3', path)
-os.system("start "+path+"\\ai_output.mp3")
+shutil.move('ai_output.mp3', ai_path)
+os.system("start "+ai_path+"\\ai_output.mp3")
+
+send_email(sender_email="####",
+        sender_password="####", #<-- use app password
+        recipient_email='####',
+        subject="Ai Output",
+        message='ai output',
+        attachment_path=ai_path+"\\ai_output.mp3"
+)
+
+audio = MP3(ai_path+"\\ai_output.mp3")
+a_l = audio.info.length
+
+time.sleep(a_l)
+os.remove(ai_path+"\\ai_output.mp3")
--- test.py
+++ test.py
@@ -1,5 +1,3 @@
-# import webbrowser
-# webbrowser.open("C:\\Users\\zombi\\OneDrive\\Desktop\\ai_call\\ai_call\\audios\\ai_output.mp3")
-
-import os
-os.system("start C:\\Users\\zombi\\OneDrive\\Desktop\\ai_call\\ai_call\\audios\\ai_output.mp3")
+from mutagen.mp3 import MP3
+audio = MP3("C:\\Users\\zombi\\OneDrive\\Desktop\\ai_call\\ai_call\\elsework\\ai_output.mp3")
+print(audio.info.length)
