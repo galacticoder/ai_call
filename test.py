508a4e02bc8dd62dee538322037e30f0985d8482
galactic <galacticoder@gmail.com>
2024-02-03 02:45:17 -0500

    updated to move audio file to folder and open it automatically when the script is executed

---
+++ test.py
@@ -0,0 +1,5 @@
+# import webbrowser
+# webbrowser.open("C:\\Users\\zombi\\OneDrive\\Desktop\\ai_call\\ai_call\\audios\\ai_output.mp3")
+
+import os
+os.system("start C:\\Users\\zombi\\OneDrive\\Desktop\\ai_call\\ai_call\\audios\\ai_output.mp3")
